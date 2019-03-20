5b94cffd9f05119df6476b38a528a48d56d7bbff
Gangarajukoti <43810755+Gangarajukoti@users.noreply.github.com>
2019-03-20 22:13:12 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -3,8 +3,8 @@ def gcd(z,y):
         return z
     else:
         return gcd(y,z%y)
-z=int(input("enter first number:"))
-y=int(input("enter second number:"))
+z=int(input("enter first number of z:"))
+y=int(input("enter second number of y:"))
 GCD=gcd(z,y)
 print("GCD is: ")
 print(GCD)
